e4e52bc3036d89835d66c34bdad585920781c1b8
WolfHoward <wolfahoward@outlook.com>
2017-07-05 20:27:18 -0500

    Rename home page to registration, refactor registration view to use a template

---
+++ registration/tests.py
@@ -0,0 +1,17 @@
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+from django.test import TestCase
+
+from registration.views import home_page
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'user_registration.html')
--- table_wisdom/urls.py
+++ table_wisdom/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from register import views
+from registration import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
