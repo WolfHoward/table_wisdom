943965fa4dfcac304cb2c9da6c9b7b7fb9c730d8
WolfHoward <wolfahoward@outlook.com>
2017-07-05 22:44:04 -0500

    Added functional tests for musical artist record.

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,12 @@ class NewUserTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_user_table(self, row_text):
+        table = self.browser.find_element_by_id('id_user_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_take_user_info_and_retrieve_it_later(self):
         ## Jamie has heard about a matchmaking app. She goes to its homepage
         self.browser.get('http://localhost:8000/user_registration/')
@@ -36,11 +42,7 @@ class NewUserTest(unittest.TestCase):
         # at the top of the page
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_discipline_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('Discipline: Educator', [row.text for row in rows])
-
+        self.check_for_row_in_user_table('Discipline: Educator')
         q_count = self.browser.find_element_by_id('id_q_count')
         print(q_count.text)
         self.assertEqual("1/3 Questions", q_count.text)
@@ -48,12 +50,18 @@ class NewUserTest(unittest.TestCase):
         # musical artist(s) [4 at most]".
         # inputbox = self.browser.find_element_by_id('id_discipline')
         # She types in "The Rolling Stones" and presses enter
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_music_artist')
+        inputbox.send_keys('Arctic Monkeys')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page now shows her field of interest, as well as her favorite artist
+        self.check_for_row_in_user_table('Discipline: Educator')
+        self.check_for_row_in_user_table('Favorite Musical Artists: Arctic Monkeys')
 
         # Finally, Jamie is asked to name her favorite sports
         # She enters Baseball
+        self.fail('Finish the test!')
 
         # The page proudly displays 3/3 questions completed and shows
         # another user that shares some answers with her.
