e1ecede895cf7d4398268d89534ede48be741a96
WolfHoward <wolfahoward@outlook.com>
2017-07-05 22:58:10 -0500

    Model for user profile and migrations.

--- registration/tests.py
+++ registration/tests.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 
+from registration.models import Item
 from registration.views import user_registration
 
 
@@ -20,3 +21,22 @@ class HomePageTest(TestCase):
         response = self.client.post('/user_registration/', data={'discipline_text': 'A new discipline'})
         self.assertIn('A new discipline', response.content.decode())
         self.assertTemplateUsed(response, 'user_registration.html')
+
+class ItemModelTest(TestCase):
+
+    def test_sasving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
